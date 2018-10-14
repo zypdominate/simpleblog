1c99b8a2dd7d02a0902d9b05ff1da4700aa7515c
zypdominate <1563117592@qq.com>
2018-10-14 23:18:01 +0800

    easy template

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import  include,url
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    url(r'^$', views.post_list)
+    url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$',views.post_detail,name='detail'),  # http://127.0.0.1:8000/post/1/
 ]
