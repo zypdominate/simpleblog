f9e4f6162fc1791811cdee2ffdd4ee9f616ab372
zypdominate <1563117592@qq.com>
2018-11-01 23:46:32 +0800

    edit article

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import  include,url
+from django.conf.urls import url
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
     url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$',views.post_detail,name='detail'),  # http://127.0.0.1:8000/post/1/
+    url(r'^post/(?P<pk>[0-9]+)/$',views.post_detail,name='post_detail'),  # http://127.0.0.1:8000/post/1/
+    url(r'^post/new/$',views.post_new,name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import  Post
+from .forms import PostForm
+
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -10,6 +14,34 @@ def post_list(request):
 def  post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    # form =PostForm()
+    # return render(request, 'blog/post_edit.html',{'form':form})
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog:detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 
 
